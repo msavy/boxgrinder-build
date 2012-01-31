4f8d9e874b288d27816decce5beba4396c73f84e
Marc Savy <msavy@redhat.com>
2012-01-31 07:38:16 +0000

    Add GPL headers to new spec files
    Fix doc layout in FSObserver
    
    Work around weird rspec bugginess
    
    Trivial layout fixes

--- spec/util/permissions/fs-observer-spec.rb
+++ spec/util/permissions/fs-observer-spec.rb
@@ -1,4 +1,22 @@
-require 'set'
+#
+# Copyright 2012 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+require 'boxgrinder-build/util/permissions/fs-observer'
 
 module BoxGrinder
   describe FSObserver do
@@ -74,7 +92,6 @@ module BoxGrinder
       end
       
       context ":command=:stop_capture" do
-
         let(:update_a){ path_update(subject, '/a/path') }  
         let(:update_b){ path_update(subject, '/b/path') }        
 
@@ -116,13 +133,11 @@ module BoxGrinder
         it "should ignore any paths that no longer exist" do
           subject.stub(:change_user)
 
-          File.stub(:exist?).and_return(true, false)
+          File.stub(:exist?).and_return(false)
           
-          FileUtils.should_receive(:chown_R).
-              with('some-usr', 'some-grp', '/a/path').once
+          FileUtils.should_not_receive(:chown_R)
 
           update_a
-          update_b
           
           subject.update({ :command => :stop_capture })
         end
